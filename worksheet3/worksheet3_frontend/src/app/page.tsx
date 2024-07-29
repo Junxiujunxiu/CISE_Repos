5c7b1a9a0f5a75551cad1e8b8f683f3205aaaaa5
Junxiu Wu <rst2479@autuni.ac.nz>
2024-07-29 20:48:31 +1200

    created the book app.
    Set up the front end with next.js
    Set up the back end with nest.js
    Set up the database with Mongodb atlas

---
+++ worksheet3/worksheet3_frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import ShowBookList from "../components/ShowBookList"; // Adjust the path as necessary
+
+export default function Home() {
+  return (
+    <main>
+      <ShowBookList />
+    </main>
+  );
+}
